7071bcbec0fcc22ef97b29eda20def41cc78960e
giszak <gaboradrian.iszak@goto.com>
2026-04-26 13:44:39 +0200

    update pocketbase

--- src/utils/pocketbase.js
+++ src/utils/pocketbase.js
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase/cjs'
 
-const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'https://prism-visuals.pockethost.io/')
+const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL || 'http://178.105.33.153:8090')
 
 // Enable auto-cancellation for realtime subscriptions
 pb.autoCancellation(false)
